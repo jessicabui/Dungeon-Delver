5b3fb5e184af71d0c6a13c92ddce39bc09d236e4
jessicabui <jessicabui97@gmail.com>
2019-04-30 15:30:21 -0400

    Initial commit

---
+++ Assets/__Scripts/GridMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMove : MonoBehaviour {
+
+      private IFacingMover mover;
+
+      void Awake()
+    {
+        mover = GetComponent<IFacingMIFacingMover>() ;
+
+    }
+
+
+}
