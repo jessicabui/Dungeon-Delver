907c05c52230e3adc9a8adbc64cde33df84a1d42
jessicabui <jessicabui97@gmail.com>
2019-05-01 20:15:27 -0400

    Finished Game

--- Assets/__Scripts/Dray.cs
+++ Assets/__Scripts/Dray.cs
@@ -10,11 +10,14 @@ public class Dray : MonoBehaviour, IFacingMover {
     public float speed = 5;
     public float attackDuration = 0.25f;
     public float attackDelay = 0.5f;
+    public float transitionDelay = 0.5f;
 
     [Header("Set Dynamically")]
     public int dirHeld = -1;
     public int facing = 1;
     public eMode mode = eMode.idle;
+    private float transitionDone = 0;
+    private Vector2 transitionPos;
 
     private float timeAtkDone = 0;
     private float timeAtkNext = 0;
@@ -40,100 +43,157 @@ public class Dray : MonoBehaviour, IFacingMover {
 
     void Update()
     {
-        dirHeld = -1;
-        //if (Input.GetKey(KeyCode.RightArrow)) dirHeld = 0;
-        //if (Input.GetKey(KeyCode.UpArrow)) dirHeld = 1;
-        //if (Input.GetKey(KeyCode.LeftArrow)) dirHeld = 2;
-        //if (Input.GetKey(KeyCode.DownArrow)) dirHeld = 3;
 
-        for(int i=0; i<4; i++)
+        if (mode == eMode.transition)
         {
-            if (Input.GetKey(keys[i])) dirHeld = i;
-        }
+            rigid.velocity = Vector3.zero;
+            anim.speed = 0;
+            roomPos = transitionPos;
+            if (Time.time < transitionDone) return;
 
-        if (Input.GetKeyDown(KeyCode.Z) && Time.time >= timeAtkNext)
-        {
-            mode = eMode.attack;
-            timeAtkDone = Time.time + attackDuration;
-            timeAtkNext = Time.time + attackDelay;
-        }
-
-        if(Time.time >= timeAtkDone)
-        {
             mode = eMode.idle;
         }
 
-        if(mode != eMode.attack)
         {
-            if(dirHeld == -1)
+            dirHeld = -1;
+            //if (Input.GetKey(KeyCode.RightArrow)) dirHeld = 0;
+            //if (Input.GetKey(KeyCode.UpArrow)) dirHeld = 1;
+            //if (Input.GetKey(KeyCode.LeftArrow)) dirHeld = 2;
+            //if (Input.GetKey(KeyCode.DownArrow)) dirHeld = 3;
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (Input.GetKey(keys[i])) dirHeld = i;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Z) && Time.time >= timeAtkNext)
+            {
+                mode = eMode.attack;
+                timeAtkDone = Time.time + attackDuration;
+                timeAtkNext = Time.time + attackDelay;
+            }
+
+            if (Time.time >= timeAtkDone)
             {
                 mode = eMode.idle;
             }
-            else
+
+            if (mode != eMode.attack)
             {
-                facing = dirHeld;
-                mode = eMode.move;
+                if (dirHeld == -1)
+                {
+                    mode = eMode.idle;
+                }
+                else
+                {
+                    facing = dirHeld;
+                    mode = eMode.move;
+                }
+            }
+
+            Vector3 vel = Vector3.zero;
+
+            switch (mode)
+            {
+                case eMode.attack:
+                    anim.CrossFade("Dray_Attack_" + facing, 0);
+                    anim.speed = 0;
+                    break;
+                case eMode.idle:
+                    anim.CrossFade("Dray_Walk_" + facing, 0);
+                    anim.speed = 0;
+                    break;
+                case eMode.move:
+                    anim.CrossFade("Dray_Walk_" + facing, 0);
+                    anim.speed = 1;
+                    break;
+            }
+
+            if (dirHeld > -1) vel = directions[dirHeld];
+
+            ////switch (dirHeld)
+            ////{
+            ////    case 0:
+            ////        vel = Vector3.right;
+            ////        break;
+
+            ////    case 1:
+            ////        vel = Vector3.up;
+            ////        break;
+
+            ////    case 2:
+            ////        vel = Vector3.left;
+            ////        break;
+
+            ////    case 3:
+            ////        vel = Vector3.down;
+            ////        break;
+            ////}
+
+            rigid.velocity = vel * speed;
+
+            //if (dirHeld == -1)
+            //{
+            //    anim.speed = 0;
+            //}
+            //else
+            //{
+            //    anim.CrossFade("Dray_Walk_" + dirHeld, 0);
+            //    anim.speed = 1;
+            //}
+        }
+
+    }     void LateUpdate()
+        {
+            Vector2 rPos = GetRoomPosOnGrid(0.5f);
+            int doorNum;
+            for (doorNum = 0; doorNum < 4; doorNum++)
+            {
+                if (rPos == InRoom.DOORS[doorNum])
+                {
+                    break;
+                }
+            }
+
+            if (doorNum > 3 || doorNum != facing) return;
+
+            Vector2 rm = roomNum;
+
+            switch (doorNum)
+            {
+                case 0:
+                    rm.x += 1;
+                    break;
+                case 1:
+                    rm.y += 1;
+                    break;
+                case 2:
+                    rm.x -= 1;
+                    break;
+                case 3:
+                    rm.y -= 1;
+                    break;
+
+            }
+
+            if(rm.x >= 0 && rm.x <= InRoom.MAX_RM_X)
+            {
+                if(rm.y >= 0 && rm.y <= InRoom.MAX_RM_Y)
+                {
+                    roomNum = rm;
+                    transitionPos = InRoom.DOORS[(doorNum + 2) % 4];
+                    roomPos = transitionPos;
+                    mode = eMode.transition;
+                    transitionDone = Time.time + transitionDelay;
+                }
             }
         }
-
-        Vector3 vel = Vector3.zero;
-
-        switch (mode)
-        {
-            case eMode.attack:
-                anim.CrossFade("Dray_Attack_" + facing, 0);
-                anim.speed = 0;
-                break;
-            case eMode.idle:
-                anim.CrossFade("Dray_Walk_" + facing, 0);
-                anim.speed = 0;
-                break;
-            case eMode.move:
-                anim.CrossFade("Dray_Walk_" + facing, 0);
-                anim.speed = 1;
-                break;
-        }
-
-        if (dirHeld > -1) vel = directions[dirHeld];
-
-        ////switch (dirHeld)
-        ////{
-        ////    case 0:
-        ////        vel = Vector3.right;
-        ////        break;
-
-        ////    case 1:
-        ////        vel = Vector3.up;
-        ////        break;
-
-        ////    case 2:
-        ////        vel = Vector3.left;
-        ////        break;
-
-        ////    case 3:
-        ////        vel = Vector3.down;
-        ////        break;
-        ////}
-
-        rigid.velocity = vel * speed;
-
-        //if (dirHeld == -1)
-        //{
-        //    anim.speed = 0;
-        //}
-        //else
-        //{
-        //    anim.CrossFade("Dray_Walk_" + dirHeld, 0);
-        //    anim.speed = 1;
-        //}
-    }
-
-    public int GetFacing()
+public int GetFacing()
     {
         return facing;
     }
 
-    public bool moving
+   public bool moving
     {
         get
         {
